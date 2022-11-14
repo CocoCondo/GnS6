2c838cd40eeee4c55ecb211a4bcc3e895fea8bf7
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2022-11-14 20:17:35 -0300

    Coso

--- src/Library/Reloj.cs
+++ src/Library/Reloj.cs
@@ -8,10 +8,10 @@ namespace Full_GRASP_And_SOLID;
         public void TimeOut()
         {
             Console.WriteLine("Timer: Calentitas las milanesas.");
+            _adaptee.SetCooked();
         }
         public Reloj(Recipe adaptee)
         {
             this._adaptee = adaptee;
-            _adaptee.SetCooked();
         }
     }
